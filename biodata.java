9d914e26b49cbf798ce9fb670c6ef8f1748ffcbe
13020180014 <61999864+13020180014@users.noreply.github.com>
2020-03-16 22:24:33 +0800

    Add files via upload

---
+++ biodata.java
@@ -0,0 +1,27 @@
+/* 
+   nama : nabilatul raham 
+    tgl : 10 maret
+    waktu : 10.00
+*/
+public class biodata {
+	public static void main (String [] args){
+
+	String Nama = "nabilatul rahma";
+	String Nim = "13020180014";
+	String jurusan = "teknik informatika";
+	String fakultas = " ilmu komputer";
+	
+
+	System.out.println("Nama = "+Nama);
+	System.out.println("Nim   =" +Nim);
+	System.out.println("Jurusan  ="+jurusan);
+	System.out.println("Fakultas ="+fakultas);
+	
+
+
+}
+
+
+}
+
+	
